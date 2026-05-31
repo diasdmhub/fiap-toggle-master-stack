566dacb7ca5259c2cfb7021ee0fb62704321956b
diasdm <diasdm@diasdm.com>
2026-05-31 19:44:56 -0300

    Patch - atualização do storageclass no módulo eks

--- modules/eks/eks.tf
+++ modules/eks/eks.tf
@@ -216,3 +216,28 @@ resource "aws_eks_addon" "ebs_csi" {
     aws_iam_role_policy_attachment.ebs_csi_AmazonEBSCSIDriverPolicy
   ]
 }
+
+# StorageClass padrão - gp3 via EBS CSI Driver
+resource "kubernetes_storage_class_v1" "gp3" {
+  metadata {
+    name = "gp3"
+    annotations = {
+      # Marca como StorageClass padrão do cluster
+      "storageclass.kubernetes.io/is-default-class" = "true"
+    }
+  }
+
+  # Provisionador do EBS CSI Driver
+  storage_provisioner    = "ebs.csi.aws.com"
+  reclaim_policy         = "Delete"
+  volume_binding_mode    = "WaitForFirstConsumer"
+  allow_volume_expansion = true
+
+  parameters = {
+    type      = "gp3"
+    fsType    = "ext4"
+    encrypted = "true"
+  }
+
+  depends_on = [aws_eks_addon.ebs_csi]
+}
